27b4567f008e92ac70f9acf39ab74a387b437d91
Vishnu <edlavishnu@aol.com>
2026-05-10 16:12:36 +0530

    changed the database name

--- home/database.py
+++ home/database.py
@@ -5,7 +5,7 @@ import psycopg2
 def connect_to_database():
     try:
         conn = psycopg2.connect(
-            host="127.0.0.1",   # use 127.0.0.1 on Windows
+            host="postgresql",   # use 127.0.0.1 on Windows
             port=5432,          # Postgres default port
             user="vishnu",
             password="bichu@#123",
